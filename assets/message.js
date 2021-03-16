89292c3701ec8c03a216998beb8137744af05c64
Wbwren <wren233@gmail.com>
2021-03-16 11:11:02 +0000

    feat: add message pop up

---
+++ assets/message.js
@@ -0,0 +1,11 @@
+function displayIntroInfo () {
+    $('#messageContainer').html(
+        `
+        <h2>Did you know?</h2>
+        <p>Polystyrene is made up of 98 per cent air and 2 per cent plastic and thus has a high volume/low weight ratio, 
+        so even a small quantity of polystyrene can rapidly fill a bin or container.</p>
+        `
+    )
+}
+
+displayIntroInfo ()
